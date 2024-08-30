cec96848136331b23022a237804b50cb539405e0
Rohitaswa Pradhan <rohitaswa96@gmail.com>
2024-08-30 15:50:01 +0530

    Designed management screen

---
+++ src/components/PersonCard/PersonCard.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import linkedin_icon_full from '../../assets/images/linkedin_icon_full.png';
+
+export const PersonCard = (person) => {
+    return (
+        <div class="card person-card">
+            <div className='d-flex justify-content-center person-image-div'>
+                <img src={person?.imageLink} class="person-image-rounded" alt={`${person?.name} image`} />
+            </div>
+            <div class="person-card-body">
+                <h5 class="card-title">{person?.name}</h5>
+                <p class="card-text">{person?.about}</p>
+            </div>
+            <div className='d-flex justify-content-center'>
+                <Link to={person?.linkedinLink} >
+                    <img src={linkedin_icon_full} width={98} height={'auto'} />
+                </Link>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/Management/ManagementScreen.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState, } from "react";
+import Banner from "../../components/Banner";
+import FooterSouthsore from "../../components/FooterSouthsore";
+import { Header } from "../../components/Header";
+import logoLink from '../../assets/images/logo.svg';
+import NavBarSouthsore from "../../components/NavBarSouthshore";
+import management_person_1 from "../../assets/images/management_person_1.png";
+import management_person_2 from "../../assets/images/management_person_2.png";
+import management_person_3 from "../../assets/images/management_person_3.png";
+import { PersonCard } from "../../components/PersonCard/PersonCard";
+
+
+const ManagementScreen = () => {
+
+    const managementPeopleDetails = [
+        {
+            name: "Jakob Spence",
+            about: "Maecenas euismod arcu leo ante consequat. Orci fermentum euismod nascetur, fermentum hendrerit lobortis. Maximus ullamcorper elit pellentesque faucibus purus, ornare orci himenaeos. Quisque ex cubilia; feugiat mauris ex magnis est dapibus. Hendrerit facilisi metus lacus sit nisi. Sagittis mauris cursus suscipit rutrum id vestibulum pretium dis class. Amet ut feugiat pellentesque sociosqu commodo",
+            linkedinLink: "#",
+            imageLink: management_person_1
+        },
+        {
+            name: "Nehemiah Berry",
+            about: "Viverra cursus ante penatibus euismod gravida suscipit. Eu mus vel posuere; nullam nam ultricies accumsan. Euismod adipiscing tristique aenean nunc maximus litora. Porttitor mauris bibendum, efficitur parturient sodales mus donec suspendisse. Felis nibh donec lacus nisi lobortis mollis potenti. Elit leo lectus inceptos tristique",
+            linkedinLink: "#",
+            imageLink: management_person_2
+        },
+        {
+            name: "Madisyn Andersen",
+            about: "Integer venenatis facilisi bibendum ante enim nascetur. Vitae ipsum rutrum varius sollicitudin gravida ridiculus. Quis id duis metus vel dignissim mollis aliquam. Auctor porta vivamus enim rhoncus lacus eget. Eros facilisis convallis etiam nisi semper tristique. Pretium velit platea venenatis molestie mauris mollis. Placerat libero ad Inceptos nunc suscipit penatibus hac tincidunt lorem arcu",
+            linkedinLink: "#",
+            imageLink: management_person_3
+        }
+    ]
+
+    return (
+        <>
+            <div class="container">
+                <Header
+                    logoLink={logoLink}
+                    logoWidth={55}
+                    logoHeight={54}
+                />
+                <NavBarSouthsore />
+
+                <div className="d-flex justify-content-center mt-3">
+                    <h1>Management</h1>
+                </div>
+                <div className="d-flex person-list align-items-center gap-3">
+                    {
+                        managementPeopleDetails.map((person, index) => {
+                            return (
+                                <PersonCard 
+                                    key={index}
+                                    name={person?.name}
+                                    about={person?.about}
+                                    imageLink={person?.imageLink}
+                                />
+                            )
+
+                        })
+                    }
+                </div>
+            </div>
+
+            <FooterSouthsore />
+        </>
+    )
+}
+
+export default ManagementScreen;
