5f21545e427e6ad70e48eaba21ba0a308f69f8ee
rohitaswa-springriver <rohitaswa.pradhan@springandriver.com>
2024-09-11 13:19:17 +0530

    Resolved merge conflicts

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -12,13 +12,11 @@ import AdvisoryScreen from './pages/Advisory/AdvisoryScreen';
 import JurisAbout from './pages/JurisAbout/JurisAbout';
 import PublishedTitlesScreen from './pages/PublishedTitlesScreen';
 import PublishWithJuris from './pages/PublishWithJuris';
-<<<<<<< HEAD
 import AcquisitionScreen from './pages/Acquisition/Acquisition';
-=======
 import PublishWithUs from './pages/PublishWithUs';
 import Enquiry from './pages/Enquiry';
 import OpenAccess from './pages/OpenAccess';
->>>>>>> master
+
 const AppRouter=()=> {
 
   return (
@@ -27,21 +25,12 @@ const AppRouter=()=> {
             <Route exact path="/" Component={HomeScreen} />
             <Route exact path="/leadership/management" Component={ManagementScreen} />
             <Route exact path="/leadership/advisory" Component={AdvisoryScreen} />
-<<<<<<< HEAD
             <Route exact path="/leadership/acquisition" Component={AcquisitionScreen} />
-
-            <Route exact path="/juris/about" Component={JurisAbout} />
-=======
->>>>>>> master
-
             <Route exact path="/juris/about" Component={JurisAbout} />
             <Route exact path="/juris/publishedtitle" Component={PublishedTitlesScreen} />
             <Route exact path="/juris/publishwithjuris" Component={PublishWithJuris} />
-
             <Route exact path="/thomson/publishwithus" Component={PublishWithUs} />
-
             <Route exact path="/representation/enquiry" Component={Enquiry} />
-
             <Route exact path="/openaccess" Component={OpenAccess} />
 
             <Route exact path="/admin/*" Component={DefaultPage}/>
