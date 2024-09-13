e2b6e355092ac497d164243c8f05510f2838a8b2
Jafrin parvin <jafrinparvin2002@gmail.com>
2024-09-13 20:00:36 +0530

    Brills Page started

--- src/pages/Brills.js
+++ src/pages/Brills.js
@@ -29,29 +29,29 @@ const Brills = () => {
                             <div className="card">
                                 <div className="card-body-list p-5">
                                     <div className=" d-flex justify-content-between title_width">
-                                        <img src={newspaper} height={25} width={25}/>
-                                    <div className="title">Journals</div>
+                                        <img className="me-3" src={newspaper} height={25} width={25} />
+                                        <div className="brill_title">Journals</div>
                                     </div>
                                     <ul className="ms-2">
-                                        <li className="marker">Over <span style={{ fontWeight: '700'}}>300+</span> journals published annually</li>
+                                        <li className="marker">Over <span style={{ fontWeight: '700' }}>300+</span> journals published annually</li>
                                         <li className="marker">Backfiles Access: Available from 2000 onwards</li>
                                         <li className="marker">Perpetual access available for purchase</li>
                                         <li className="marker">Subscription-based e-journals</li>
                                         <li className="marker">Indexed in Scopus and Web of Science</li>
                                         <li className="marker">Full-texts available in PDF format</li>
                                         <li className="marker">Compliant with CROSSREF, ATHENS, SHIBBOLETH, and COUNTER standards</li>
-                                        
+
                                     </ul>
-                                    
+
                                 </div>
                             </div>
                         </div>
                         <div className="col-sm-6">
-                        <div className="card">
+                            <div className="card heighgt:400">
                                 <div className="card-body-list p-5">
-                                    <div className="d-flex justify-content-between title_width">
-                                        <img src={book} height={25} width={25}/>
-                                    <div className="title">Books and E-Books</div>
+                                    <div className="d-flex ">
+                                        <img className="me-3" src={book} height={25} width={25} />
+                                        <div className="brill_title">Books and E-Books</div>
                                     </div>
                                     <ul className="ms-2">
                                         <li className="marker">Publishing approximately 1,200 new monograph/series titles annually</li>
@@ -62,31 +62,31 @@ const Brills = () => {
                                         <li className="marker">No DRM restrictions</li>
                                         <li className="marker">Unlimited users and downloads — buy one book, allow multiple simultaneous users</li>
                                         <li className="marker">Usage reports available for institutions</li>
-                                        
+
                                     </ul>
-                                    
+
                                 </div>
                             </div>
                         </div>
                         <div className="row mt-5">
-                        <div className="col-sm-6">
-                        <div className="card">
-                                <div className="card-body-list p-5">
-                                    <div className="d-flex justify-content-between title_width">
-                                        <img src={dollar} height={25} width={25}/>
-                                    <div className="title">Purchase Models</div>
+                            <div className="col-sm-6">
+                                <div className="card">
+                                    <div className="card-body-list p-5">
+                                        <div className="d-flex title">
+                                            <img className="me-3" src={dollar} height={25} width={25} />
+                                            <div className="brill_title">Purchase Models</div>
+                                        </div>
+                                        <ul className="ms-2">
+                                            <li className="marker">Pick and Choose</li>
+                                            <li className="marker">Annual Collections</li>
+                                            <li className="marker">Evidence-Based Acquisition (EBA) Model</li>
+
+
+                                        </ul>
+
                                     </div>
-                                    <ul className="ms-2">
-                                        <li className="marker">Pick and Choose</li>
-                                        <li className="marker">Annual Collections</li>
-                                        <li className="marker">Evidence-Based Acquisition (EBA) Model</li>
-                                        
-                                        
-                                    </ul>
-                                    
                                 </div>
                             </div>
-                        </div>
                         </div>
                     </div>
                 </div>
